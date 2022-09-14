3c0881a835f2472e82600d8d5ef277023c712083
Muhammad Awais <muhammad.awais4@afiniti.com>
2022-09-14 11:36:43 +0500

    before express chat app work

--- module-consumer.js
+++ module-consumer.js
@@ -1,5 +1,15 @@
-var module = require('./module')
-console.log(module)
-// module.myfun('aoa')
+// var module = require('./module')
+// console.log(module)
 
+// FILE READING
+var data = require('./data.json')
+// MA: why do we even need fs package if we can access the files like this??
+console.log(typeof data);
+var fs = require('fs')
+// module.myfun('aoa')
+fs.readFile('./data.json', 'utf-8', (err, data) => {
+    let objData = JSON.parse(data)
+    console.log(typeof objData)
+    console.log(objData)
+});
 
